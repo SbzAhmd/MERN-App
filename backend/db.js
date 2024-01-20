f491ce7335b06b77de7127ead7797701add37915
Shabaz <111643220+SbzAhmd@users.noreply.github.com>
2024-01-20 11:06:13 +0530

    Add files via upload

---
+++ backend/db.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://gofood:Shabaz%4021@cluster0.irpiivn.mongodb.net/gofoodmern?retryWrites=true&w=majority'
+const mongoDB = async () => {
+
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+
+        if (err) console.log("---", err);
+
+        else {
+
+            console.log("connected");
+            const fetched_data = await mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray(async function (err, data) {
+
+                const foodCategory = await mongoose.connection.db.collection("foodCategory");
+                foodCategory.find({}).toArray(function (err, catData) {
+
+                    if (err) console.log(err);
+                    else {
+
+                        global.food_items = data;
+                        global.foodCategory = catData;
+
+                    }
+
+                })
+                // if(err) console.log(err);
+                // else {
+                //     global.food_items =  data;
+
+                // }
+            })
+
+        }
+
+    });
+
+}
+
+module.exports = mongoDB;
